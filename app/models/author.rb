c2bf00f92170d7011d023c20eeeaa0f8891ad09f
Taek Jin Han <tj.han@yale.edu>
2021-07-19 19:00:55 -0400

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
-end
+    validates :name, presence: true
+    validates :email, uniqueness: true
+  end
+  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  end
+  
